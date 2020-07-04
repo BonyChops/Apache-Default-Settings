5ddc5836c10ed4d503efb71c066d02e91fcee93b
BonyChops <office.kentarou@gmail.com>
2020-07-04 21:01:47 +0900

    Edit: fixed some bugs

---
+++ README.md
@@ -0,0 +1,6 @@
+# Apache-Default-Settings
+## What this will do
+- Install apache2 and latest php
+- Change document root from /var/www to ~/html
+## Support
+- Ubuntu 18.04+
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ cd ~/
 set +e
 mkdir ~/html
 set -e
-sudo sed -e "s/<Directory \/var\/www\/html>/<Directory \/home\/$USERNAME\/html>/"  /etc/apache2/apache2.conf.orig > ~/apache2.conf
+sudo sed -e "s/<Directory \/var\/www>/<Directory \/home\/$USERNAME\/html>/"  /etc/apache2/apache2.conf.orig > ~/apache2.conf
 sudo sed -e "s/Options Indexes FollowSymLinks/Options FollowSymLinks/"   ~/apache2.conf > ~/apache2_1.conf
 sudo sed -e "s/AllowOverride None/AllowOverride All/"   ~/apache2_1.conf > ~/apache2_2.conf
 sudo cp ~/apache2_2.conf /etc/apache2/apache2.conf
@@ -18,3 +18,5 @@ sudo sed -e "s/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/$USERNAME\/htm
 sudo cp ~/000-default.conf /etc/apache2/sites-available/000-default.conf
 echo "Restarting apache2..."
 sudo service apache2 restart
+rm ~/apache2*.conf
+rm 000-default.conf
